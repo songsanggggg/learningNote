584d99fa491c24d6b82eae2effcd35b1f428b236
songsanggggg <bxgh6_xivkv71ra8@163.com>
2024-09-15 15:10:33 +0800

    2024/9/15

---
+++ CPP/example/string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+    // char rabbit[4] = {'A', '\0', 'C', 'D'};
+    // char rabbit[] = "Hello world";
+    std::string rabbit = "Hello World";
+    cout << rabbit << endl;
+
+    return 0;
+}
