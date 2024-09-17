a00ecad975ae76ec1b3cb611d8967130b8b904a9
songsanggggg <bxgh6_xivkv71ra8@163.com>
2024-09-17 17:58:52 +0800

    2024/9/17

--- CPP/example/pointer.cpp
+++ CPP/example/pointer.cpp
@@ -18,14 +18,33 @@ int main()
         std::cout << num << std::endl;
         *p2 = 30;
         std::cout << num << std::endl; */
-    Student stu = {"Yu", 2000, true};
-    Student *pStu = &stu;
-    pStu->male = false;
-    std::cout << "male:" << (pStu->male ? "man" : "woman") << std::endl;
-    (*pStu).year = 2001;
-    std::cout << "year:" << (*pStu).year << std::endl;
-    strncpy(pStu->name, "Li", 4);
-    std::cout << "name:" << pStu->name << std::endl;
+    /*     Student stu = {"Yu", 2000, true};
+        Student *pStu = &stu;
+        pStu->male = false;
+        std::cout << "male:" << (pStu->male ? "man" : "woman") << std::endl;
+        (*pStu).year = 2001;
+        std::cout << "year:" << (*pStu).year << std::endl;
+        strncpy(pStu->name, "Li", 4);
+        std::cout << "name:" << pStu->name << std::endl; */
+
+    /* Student stu[128];
+    std::cout << &stu << std::endl;
+    std::cout << stu << std::endl;
+    std::cout << &stu[0] << std::endl;
+
+    Student *p = stu;
+    p[0].male = false;
+    std::cout << "male:" << (p[0].male ? "male" : "female") << std::endl; */
+
+    /* int nums[4] = {1, 2, 3, 4};
+
+    int *p = nums;
+    p++;
+    *p = 30;
+    for (int i = 0; i < 4; i++)
+    {
+        std::cout << "nums[" << i << "]: " << nums[i] << std::endl;
+    } */
 
     return 0;
 }
