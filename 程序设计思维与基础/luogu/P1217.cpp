eff0978e14bc6c0be8ef935bf3acbc1103ccbfdb
songsanggggg <bxgh6_xivkv71ra8@163.com>
2024-10-10 20:40:28 +0800

    2024/10/10

--- 程序设计思维与基础/luogu/P1217.cpp
+++ 程序设计思维与基础/luogu/P1217.cpp
@@ -1,7 +1,56 @@
 #include <iostream>
-#include <cmath>
-
+#include <string>
 using namespace std;
+
+bool isPrime(int number);
+
+int main()
+{
+    unsigned long long palindromes[1000000];
+    unsigned long long count = 0, min = 0, max = 0;
+    cin >> min >> max;
+
+    for (int i = 1; i <= 9; i += 2)
+    {
+        if (isPrime(i))
+        {
+            palindromes[count++] = i;
+        }
+    }
+
+    for (int i = 1; i <= 9999; i += 2)
+    {
+        string s = to_string(i);
+        string rev = string(s.rbegin(), s.rend());
+        unsigned long long palindrome = stoll(rev + s);
+        if (palindrome <= max && isPrime(palindrome))
+        {
+            palindromes[count++] = palindrome;
+        }
+    }
+
+    for (int i = 1; i <= 9999; i++)
+    {
+        string s = to_string(i);
+        string rev = string(s.rbegin(), s.rend());
+        for (char c = '0'; c <= '9'; c++)
+        {
+            unsigned long long palindrome = stoll(s + c + rev);
+            if (palindrome <= max && isPrime(palindrome))
+                palindromes[count++] = palindrome;
+        }
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+        if (palindromes[i] >= min && palindromes[i] <= max)
+        {
+            cout << palindromes[i] << "\n";
+        }
+    }
+
+    return 0;
+}
 
 bool isPrime(int number)
 {
@@ -15,34 +64,4 @@ bool isPrime(int number)
         }
     }
     return true;
-}
-
-bool isPalindrome(int number)
-{
-    int original = number;
-    int reversed = 0;
-
-    while (number > 0)
-    {
-        reversed = reversed * 10 + (number % 10);
-        number /= 10;
-    }
-
-    return original == reversed;
-}
-
-int main()
-{
-    int a, b;
-    cin >> a >> b;
-
-    for (int i = (floor(a / 2) * 2) + 1; i <= b; i += 2)
-    {
-        if (isPrime(i) && isPalindrome(i))
-        {
-            cout << i << endl;
-        }
-    }
-
-    return 0;
 }
